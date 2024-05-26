029a5fefca1ad01b71efed9f72433f030314962d
Brian Kimurgor <kimurgorbrian20@gmail.com>
2024-05-26 23:29:16 +0300

    designing the logo size well

--- Blogify/__init__.py
+++ Blogify/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_reuploads import UploadSet, IMAGES, configure_uploads
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
